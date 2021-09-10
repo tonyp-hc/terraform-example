c0b601d39fbf70b40d28a6bbb6d242527a5af19b
Tony Pulickal <ttp@hashicorp.com>
2021-09-10 14:39:29 -0400

    add large output example

---
+++ seed-workspace/many-pets/main.tf
@@ -0,0 +1,18 @@
+variable "test_var" {
+  description  = "Dummy variable that can be used to force a variable error."
+  type         = string
+  default      = 10
+}
+
+# Generate a change in the workspace
+resource "random_pet" "main" {
+  keepers = {
+    always = timestamp()
+  }
+  count  = 100
+  length = var.test_var
+}
+
+output "animal_sanctuary" {
+  value = random_pet.main.*.id
+}
